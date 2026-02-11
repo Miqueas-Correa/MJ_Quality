bcb326491da753f753301e0a9e673abca3bf63e7
Miqueas Correa <miki.mate.tizi@gmail.com>
2026-02-11 16:44:24 -0300

    estilos base

--- mi-frontend/src/main.jsx
+++ mi-frontend/src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import theme from './utils/theme.js'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <themeProvider theme={theme}>
+    <CssBaseline />
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </themeProvider>
 )
